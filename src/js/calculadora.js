46a1f677a2e165271222b969f8556a1e758f4e81
cemf <kaikelmfabricio@gmail.com>
2021-09-18 23:12:34 -0300

    primeiro comit de conteudo

---
+++ src/js/calculadora.js
@@ -0,0 +1,19 @@
+const Calculadora = {
+    adicionar: function(num1,num2){
+        return 0
+    },
+    subtrair: function(num1,num2){
+        return 0
+    },
+    dividir: function(num1,num2){
+        return 0
+    },
+    multiplicar: function(num1,num2){
+        return 0
+    }
+}
+
+// usado no require do node
+if(typeof module !== 'undefined' && typeof module.exports !=='undefined'){
+    module.exports = Calculadora
+}
