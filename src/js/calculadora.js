e67c77a2d8cdcdbb149fab1d9f5ca32658187e58
cemf <kaikelmfabricio@gmail.com>
2021-09-20 08:46:11 -0300

    refatorando a calculadora e ajeitado configuraçoes

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,31 +1,47 @@
 const Calculadora = {
-    adicionar: function(num1,num2){
-        if(isNaN(num1) || isNaN(num2)){
-            return 0
-        }
+    ADICAO:'+',
+    SUBTRACAO:'-',
+    DIVISAO:'/',
+    MULTIPLICACAO:'*',
+    calcular: (num1,num2,operacao)=>{
+        let resposta;
         num1 = parseFloat(num1)
         num2 = parseFloat(num2)
-        return num1 + num2;
+        if(isNaN(num1) || isNaN(num2)){
+            return 0
+        }
+    switch(operacao){
+        case Calculadora.ADICAO:
+            resposta = num1 + num2;
+            break;
+        case Calculadora.SUBTRACAO:
+            resposta = num1 - num2;
+            break;
+        case Calculadora.MULTIPLICACAO:
+            resposta = num1 * num2;
+            break;
+        case Calculadora.DIVISAO:
+            resposta = num1/num2;
+            break
+        default:
+            resposta = 0;
+    }
+    return resposta;
+    },
+    adicionar: function(num1,num2){
+        return Calculadora.calcular(num1,num2,Calculadora.ADICAO)
     },
     subtrair: function(num1,num2){
-        if(isNaN(num1) || isNaN(num2)){
-            return 0
-        }
-        return num1 - num2
+       return Calculadora.calcular(num1,num2,Calculadora.SUBTRACAO)
     },
     dividir: function(num1,num2){
-        if(num2==0){
-            return "ERRO"
-        }else if(isNaN(num1) || isNaN(num2)){
-            return 0
+        if(parseFloat(num2)===0){
+            return 'ERRO'
         }
-        return num1/num2
+        return Calculadora.calcular(num1,num2,Calculadora.DIVISAO)
     },
     multiplicar: function(num1,num2){
-        if(isNaN(num1) || isNaN(num2)){
-            return 0
-        }
-        return num1*num2
+        return Calculadora.calcular(num1,num2,Calculadora.MULTIPLICACAO)
     }
 }
 
